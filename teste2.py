19602f0fee13aeeb7f7ca58e0749b548e8443da7
stephanoumenos <s_dechecchi@hotmail.com>
2017-10-05 16:00:57 -0300

    Initial commit

---
+++ teste2.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+import time
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    ret, frame = cap.read()
+    if ret == True:
+        cv2.imshow('frame',frame)
+        cv2.waitKey(0)
+    else:
+        print("Erro, webcam nao reconhecida")
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+main()
+
