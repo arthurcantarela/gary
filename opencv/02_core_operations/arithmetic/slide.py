8360bd9b96301359d6408bb01e4e605ae58a9bc3
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-10-12 20:53:56 -0300

    todo slide.py

--- opencv/02_core_operations/arithmetic/slide.py
+++ opencv/02_core_operations/arithmetic/slide.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 import numpy as np
 
 img1 = cv2.imread('horse.jpg')
@@ -9,10 +10,12 @@ def slide_next(im1, im2):
     for i in range(10):
         dst = cv2.addWeighted(im1,(i+1)/10,im2,(1-(i+1)/10.0),0)
         cv2.imshow('image', dst)
+        time.sleep(0.1)
 
 cv2.imshow('image', img1)
-cv2.waitKey(0)
+time.sleep(1)
 slide_next(img1, img2)
-cv2.waitKey(0)
+time.sleep(1)
 slide_next(img2, img3)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
