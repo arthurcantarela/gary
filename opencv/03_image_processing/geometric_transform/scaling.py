98ef478130e59d644ca64b7cc49d11288129b8de
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-10-13 11:40:08 -0300

    done geometric transform tutorial

---
+++ opencv/03_image_processing/geometric_transform/scaling.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('opencv-logo2.png')
+
+res = cv2.resize(img,None,fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
+
+cv2.imshow('image', res)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#OR
+
+height, width = img.shape[:2]
+res = cv2.resize(img,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+
+cv2.imshow('image', res)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
