2d3e57b586f172d7292ceb08ff8b0b819058e13a
stephanoumenos <s_dechecchi@hotmail.com>
2017-10-16 21:39:37 -0200

    Mostrando video da webcam

--- teste2.py
+++ teste2.py
@@ -3,14 +3,15 @@ import cv2
 import time
 
 def main():
-    cap = cv2.VideoCapture(0)
 
-    ret, frame = cap.read()
-    if ret == True:
-        cv2.imshow('frame',frame)
-        cv2.waitKey(0)
-    else:
-        print("Erro, webcam nao reconhecida")
+    cap = cv2.VideoCapture(0)
+    while(True):
+        ret, frame = cap.read()
+        if ret == True:
+            cv2.imshow('frame',frame)
+            cv2.waitKey(1)
+        else:
+            print("Erro, webcam nao reconhecida")
 
     cap.release()
     cv2.destroyAllWindows()
