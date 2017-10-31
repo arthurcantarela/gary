0c73a1b38172abbe019866eb9f5dc20cedee6c37
stephanoumenos <s_dechecchi@hotmail.com>
2017-10-31 15:37:38 -0200

    This one is working we just have to make it work for all pics and crop them

---
+++ photos/script.sh
@@ -0,0 +1,1 @@
+opencv_createsamples -img positive/2017-10-26-15-33-1115.png -bg negative/negatives.txt -vec sample.vec -maxxangle 0.6 -maxyangle 0.1 -maxzangle 0.3 -maxxidev 100 -bgcolor 0 -bgthresh 0 -w 80 -h 40
