0b75773788b205a29e3fb25fb83664ed71ce5b6e
luizfelipemr <luizfmrego@gmail.com>
2017-10-31 15:04:07 -0200

    PWM separated for servos and motors

--- gogarygo.py
+++ gogarygo.py
@@ -15,7 +15,7 @@ G.setup(13,G.OUT) # front
 G.setup(19,G.OUT) # back
 
 # GPIOs controller for servomotor
-G.setup(3,G.OUT) # back
+G.setup(3,G.OUT)
 
 # GPIO PWM setup
 
@@ -23,11 +23,12 @@ G.setup(18,G.OUT) #PWM for motors
 G.setup(3,G.OUT) #PWM for servomotor
 
 #PWM frequency operation
-pwm = G.PWM(3,50)
-pwm = G.PWM(18,100)
+pwm_sm = G.PWM(3,50)
+pwm_m = G.PWM(18,100)
 
 #Starts PWM cycle
-pwm.start(0)
+pwm_sm.start(0)
+pwm_m.start(0)
 
 #Moves Gary for a direction
 def MoveGary(direction):
@@ -49,20 +50,20 @@ def MoveGary(direction):
     if direction == 2:
         G.output(5,False)
         G.output(6,True)
-        G.output(13,True)
-        G.output(19,False)
+        G.output(13,False)
+        G.output(19,True)
 
     #Move Right
     if direction == 6:
         G.output(5,True)
         G.output(6,False)
         G.output(13,False)
-        G.output(19,True)
+        G.output(19,False)
 
     #Move Left
     if direction == 4:
         G.output(5,False)
-        G.output(6,True)
+        G.output(6,False)
         G.output(13,True)
         G.output(19,False)
 
@@ -74,27 +75,31 @@ def DriveGary(speed):
 
     #Stay
     if speed == 0:
-        pwm.ChangeDutyCycle(0)
+        pwm_m.ChangeDutyCycle(0)
 
         #Low Speed
     if speed == 1:
-        pwm.ChangeDutyCycle(30)
+        pwm_m.ChangeDutyCycle(30)
 
             #Regular Speed
     if speed == 2:
-        pwm.ChangeDutyCycle(60)
+        pwm_m.ChangeDutyCycle(60)
 
                 #High Speed
     if speed == 3:
-        pwm.ChangeDutyCycle(100)
+        pwm_m.ChangeDutyCycle(100)
+
+
+pwm_m.stop()
 
 #Servomotor control
 def ServoControl(angle):
     duty = angle/18 + 2
     GPIO.output(3, True)
-    pwm.ChangeDutyCycle(duty)
+    pwm_sm.ChangeDutyCycle(duty)
     sleep(1)
     GPIO.output(3, False)
 
-pwm.stop()
+pwm_sm.stop()
+
 G.cleanup()
