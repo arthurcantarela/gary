e7596dd73b15f5feb4623aff6968bd0c449c3e05
stephanoumenos <s_dechecchi@hotmail.com>
2017-11-03 11:56:46 -0200

    The script now seems to be working for all pics. Will save to samples/

--- photos/script.sh
+++ photos/script.sh
@@ -1,1 +1,10 @@
-opencv_createsamples -img positive/sample1.png -bg negative/negatives.txt -vec sample.vec -maxxangle 0.6 -maxyangle 0.1 -maxzangle 0.3 -maxxidev 100 -bgcolor 0 -bgthresh 0 -w 80 -h 40
+#!/bin/bash
+
+COUNTER=0
+
+for i in $( ls positive_cropped ); do
+	opencv_createsamples -img positive_cropped/$i -bg negative/negatives.txt -vec samples/sample_$COUNTER.vec -maxxangle 0.6 -maxyangle 0.1 -maxzangle 0.3 -maxxidev 100 -bgcolor 0 -bgthresh 0 -w 80 -h 40
+	let COUNTER+=1
+done
+
+
