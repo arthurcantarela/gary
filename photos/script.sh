4af44fdc28512845adfad84ce9b6a618e9be2567
stephanoumenos <s_dechecchi@hotmail.com>
2017-11-04 01:27:08 -0200

    Agrupei os samples gerados

--- photos/script.sh
+++ photos/script.sh
@@ -3,7 +3,7 @@
 COUNTER=0
 
 for i in $( ls positive_cropped ); do
-	opencv_createsamples -img positive_cropped/$i -bg negative/negatives.dat -vec samples/sample_$COUNTER.vec -maxxangle 0.6 -maxyangle 0.1 -maxzangle 0.3 -maxxidev 100 -bgcolor 0 -bgthresh 0 -w 80 -h 40
+	opencv_createsamples -img positive_cropped/$i -bg negative/negatives.dat -vec samples/sample_$COUNTER.vec -maxxangle 0.6 -maxyangle 0.1 -maxzangle 0.3 -maxxidev 100 -bgcolor 0 -bgthresh 0 -w 20 -h 20
 	let COUNTER+=1
 done
 
