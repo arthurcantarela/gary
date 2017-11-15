89ed7aaffbd187acd8d93c66583eb50bf5a072df
艾力克 <30643012+efrizon@users.noreply.github.com>
2017-11-15 14:43:49 -0200

    Correção

--- gogarygo.py
+++ gogarygo.py
@@ -1,16 +1,15 @@
-
 import sys
 import RPi.GPIO as G
 from time import sleep
 G.setmode(G.BCM)
 
-class GarbageColectorRobot():
+class GarbageColectorRobot:
 
-    def __init__:
+    def __init__(self):
     # GPIOs controllers for left motors
 
         G.setup(5, G.OUT) # front
-        G.setup(6, G.OUT) #back
+        G.setup(6, G.OUT) # back
 
         # GPIOs controllers for right motors
 
@@ -22,19 +21,25 @@ class GarbageColectorRobot():
 
         # GPIO PWM setup
 
-        G.setup(18,G.OUT) #PWM for motors
-        G.setup(3,G.OUT) #PWM for servomotor
+        G.setup(18,G.OUT) # PWM for motors
+        G.setup(3,G.OUT) # PWM for servomotor
 
         #PWM frequency operation
-        pwm_sm = G.PWM(3,50)
-        pwm_m = G.PWM(18,100)
+        self.pwm_sm = G.PWM(3,50)
+        self.pwm_m = G.PWM(18,100)
 
         #Starts PWM cycle
-        pwm_sm.start(0)
-        pwm_m.start(0)
+        self.pwm_sm.start(0)
+    	self.pwm_m.start(0)
+
+	#Initial position for trowel
+	self.trowel(90)
 
     #Moves Gary for a direction
-    def move(direction):
+    def move(self,direction,speed):
+	
+	crawl(speed)
+
         #Stop
         if direction == 5:
             G.output(5,False)
@@ -74,35 +79,56 @@ class GarbageColectorRobot():
 
 
     #Velocity Control
-    def crawl(speed):
+    def crawl(self,speed):
 
         #Stay
         if speed == 0:
-            pwm_m.ChangeDutyCycle(0)
+            self.pwm_m.ChangeDutyCycle(0)
 
-            #Low Speed
+        #Low Speed
         if speed == 1:
-            pwm_m.ChangeDutyCycle(30)
+            self.pwm_m.ChangeDutyCycle(30)
 
-                #Regular Speed
+        #Regular Speed
         if speed == 2:
-            pwm_m.ChangeDutyCycle(60)
+            self.pwm_m.ChangeDutyCycle(60)
 
-                    #High Speed
+        #High Speed
         if speed == 3:
-            pwm_m.ChangeDutyCycle(100)
+            self.pwm_m.ChangeDutyCycle(100)
 
 
-    pwm_m.stop()
+    	self.pwm_m.stop()
 
     #Servomotor control
-    def trowel(angle):
-        duty = angle/18 + 2
-        GPIO.output(3, True)
-        pwm_sm.ChangeDutyCycle(duty)
-        sleep(1)
-        GPIO.output(3, False)
+	
+	#Reminder: Initial state - angle 90; Prepare trowel - angle 180; EAT - angle: 0; Next food - angle 90 (loop)
+	#
 
-    def sleep():
-        pwm_sm.stop()
+    def trowel(self,angle):
+        duty = angle/18 + 2
+        G.output(3, True)
+        self.pwm_sm.ChangeDutyCycle(duty)
+	sleep(1)
+
+        G.output(3, False)
+	self.pwm_sm.ChangeDutyCycle(0)
+
+    def hungry(self):
+
+	#Open
+	self.trowel(180)
+	sleep(1)
+	
+	#Eat
+	self.trowel(0)
+	sleep(1)
+
+	#Close
+	self.trowel(90)
+	sleep(1)
+
+
+    def sleep(self):
+        self.pwm_sm.stop()
         G.cleanup()
