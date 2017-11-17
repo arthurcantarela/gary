15f5135ff37d29ddf2957b05ab62e3f1e76914d1
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-11-17 21:43:31 -0200

    think its working

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-import numpy as np
-import cv2
-import lib.cam
+import os
+from lib import cam
 
 # run this script from / as python -m test.py
 
@@ -8,8 +7,9 @@ eyes = cam.Cam(0)
 dir = os.path.dirname(__file__)
 filename = os.path.join(dir, '/photos/positive/')
 for filename in os.listdir():
-    inp = cv2.imread(filename)
-    out = eyes.detect_garbage_picture(inp)
-    for i,e in enumerate(out):
-        cv2.imwrite(filename+i, e)
+    if filename.endswith(".png"):
+        inp = cv2.imread(filename, 3)
+        out = eyes.detect_garbage_picture(inp)
+        for i,e in enumerate(out):
+            cv2.imwrite(filename+str(i), e)
 eyes.close()
