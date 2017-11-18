81cf86e47ba03cbb2bb4913a30d5d24568c21e18
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-11-18 16:38:30 -0200

    fixed script

--- test.py
+++ test.py
@@ -4,12 +4,13 @@ from lib import cam
 # run this script from / as python -m test.py
 
 eyes = cam.Cam(0)
-dir = os.path.dirname(__file__)
+dir = os.getcwd()
 filename = os.path.join(dir, '/photos/positive/')
 for filename in os.listdir():
     if filename.endswith(".png"):
         inp = cv2.imread(filename, 3)
         out = eyes.detect_garbage_picture(inp)
         for i,e in enumerate(out):
-            cv2.imwrite(filename+str(i), e)
+            # cv2.imwrite(filename+str(i), e)
+            print("found 1 object in photo %s" % filename)
 eyes.close()
