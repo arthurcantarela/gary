87e49a6ab729e34b29f681a720a2daa10edce5ad
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-11-22 15:49:39 -0200

    merged gogarygo from dev-luiz

--- gogarygo.py
+++ gogarygo.py
@@ -1,16 +1,15 @@
-
 import sys
 import RPi.GPIO as G
 from time import sleep
 G.setmode(G.BCM)
 
-class GarbageColectorRobot():
+class Trowel:
 
-    def __init__:
+    def __init__(self):
     # GPIOs controllers for left motors
 
         G.setup(5, G.OUT) # front
-        G.setup(6, G.OUT) #back
+        G.setup(6, G.OUT) # back
 
         # GPIOs controllers for right motors
 
@@ -20,21 +19,39 @@ class GarbageColectorRobot():
         # GPIOs controller for servomotor
         G.setup(3,G.OUT)
 
-        # GPIO PWM setup
+        # GPIO with PWM setup for each motor
 
-        G.setup(18,G.OUT) #PWM for motors
-        G.setup(3,G.OUT) #PWM for servomotor
+        G.setup(18,G.OUT) # PWM for front-left
+        G.setup(23,G.OUT) # PWM for front-right
+        G.setup(24,G.OUT) # PWM for back-left
+        G.setup(25,G.OUT) # PWM for back-right
 
         #PWM frequency operation
-        pwm_sm = G.PWM(3,50)
-        pwm_m = G.PWM(18,100)
+        self.pwm_sm = G.PWM(3,50)
+        self.pwm_m1 = G.PWM(18,100)
+        self.pwm_m2 = G.PWM(23,100)
+        self.pwm_m3 = G.PWM(24,100)
+        self.pwm_m4 = G.PWM(25,100)
 
         #Starts PWM cycle
-        pwm_sm.start(0)
-        pwm_m.start(0)
+        self.pwm_sm.start(0)
+    	self.pwm_m1.start(0)
+        self.pwm_m2.start(0)
+        self.pwm_m3.start(0)
+        self.pwm_m4.start(0)
+
+	#Initial position for trowel
+	self.move_servo(90)
 
     #Moves Gary for a direction
-    def move(direction):
+    def move(self,direction,speed):
+
+    ###########Test if its nececessary to change the velocitys according to the move of the robot
+	self.vel_motor1(speed)
+    self.vel_motor2(speed)
+    self.vel_motor3(speed)
+    self.vel_motor4(speed)
+
         #Stop
         if direction == 5:
             G.output(5,False)
@@ -74,35 +91,53 @@ class GarbageColectorRobot():
 
 
     #Velocity Control
-    def crawl(speed):
+    def vel_motor1(self,speed):
 
-        #Stay
-        if speed == 0:
-            pwm_m.ChangeDutyCycle(0)
+        speed = 0.67*speed
+        self.pwm_m.ChangeDutyCycle(speed)
+    	self.pwm_m.stop()
 
-            #Low Speed
-        if speed == 1:
-            pwm_m.ChangeDutyCycle(30)
+    def vel_motor2(self,speed):
 
-                #Regular Speed
-        if speed == 2:
-            pwm_m.ChangeDutyCycle(60)
+        speed = 0.77*speed
+        self.pwm_m.ChangeDutyCycle(speed)
+        self.pwm_m.stop()
 
-                    #High Speed
-        if speed == 3:
-            pwm_m.ChangeDutyCycle(100)
+    def vel_motor3(self,speed):
 
+        speed = 0.92*speed
+        self.pwm_m.ChangeDutyCycle(speed)
+    	self.pwm_m.stop()
 
-    pwm_m.stop()
+    def vel_motor4(self,speed):
+
+        self.pwm_m.ChangeDutyCycle(speed)
+    	self.pwm_m.stop()
 
     #Servomotor control
-    def trowel(angle):
-        duty = angle/18 + 2
-        GPIO.output(3, True)
-        pwm_sm.ChangeDutyCycle(duty)
-        sleep(1)
-        GPIO.output(3, False)
 
-    def sleep():
-        pwm_sm.stop()
+	########Reminder: Initial state - angle 90; Prepare trowel - angle 180; EAT - angle: 0; Next food - angle 90 (loop)
+    def turn_servo(self,angle):
+        duty = angle/18 + 2
+        G.output(3, True)
+        self.pwm_sm.ChangeDutyCycle(duty)
+	sleep(1)
+
+        G.output(3, False)
+	self.pwm_sm.ChangeDutyCycle(0)
+
+    def hungry(self):
+
+	#Open
+	self.turn_servo(180)
+
+	#Eat
+	self.turn_servo(0)
+
+	#Close
+	self.turn_servo(90)
+
+
+    def sleep(self):
+        self.pwm_sm.stop()
         G.cleanup()
